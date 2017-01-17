7e23c4aafb8a6301c538e91fdb239e99a41001d2
AllanYin <Allan.jun.yin@gmail.com>
2017-01-18 00:05:59 +1100

    Dev: create initial project

---
+++ src/main/java/com/AllanYin/App.java
@@ -0,0 +1,13 @@
+package com.AllanYin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
